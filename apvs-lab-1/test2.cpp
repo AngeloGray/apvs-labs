ecf729918fd04e3e470c9229a5871d4d5e8b2dc5
AngeloGray <hasatik2001@gmail.com>
2022-12-21 16:42:21 +0300

    refactored code

--- apvs-lab-1/test2.cpp
+++ apvs-lab-1/test2.cpp
@@ -4,21 +4,21 @@
 #include <vector>
 using namespace std;
 
-const int a_size = 3;
-    //Функция для печати матриц в консоль
+    //Функции для печати матриц и векторов в консоль
     void ShowMatrix(vector<vector<float> >& a) {
     // Вывод матрицы
-        for (int i = 0; i < a_size; i++) {
-            for (int j = 0; j < a_size; j++) {
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < a.size(); j++) {
                 cout << a[i][j] << " ";
             }
             cout << "\n";
         }
+        cout << "\n";
     }
     void ShowMatrixEx(vector<vector<float> >& a) {
-        for (int i = 0; i < a_size; i++) {
-            for (int j = 0; j < a_size + 1; j++) {
-                if (a_size - j == 1) {
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = 0; j < a.size() + 1; j++) {
+                if (a.size() - j == 1) {
                     cout << a[i][j] << " | "; // разделитель основной
                     continue;                      // и расширенной матриц
                 }
@@ -26,33 +26,36 @@ const int a_size = 3;
             }
             cout << "\n";
         }
+        cout << "\n";
     }
     void ShowVector(vector<float> & a){
-        //Вывод решения системы уравнений
-        cout << "Linear equations system answer is:" << endl;
-        for (int i = 0; i < a_size; i++) {
-            cout << "X" << i << " = " << a[i] << "\n";
+        //Вывод вектора
+        for (int i = 0; i < a.size(); i++) {
+            cout << a[i] << "\n";
         }
+        cout << "\n";
+    }
+    void ShowResult(vector<vector<float>>& a) {
+        cout << "Linear equations system answer is:" << endl;
+        for (int i = 0; i < a.size(); i++) {
+            cout << "X" << i << " = " << a[i][a.size()] << "\n";
+        }
+        cout << "\n";
     }
 
-
-    // Функция для нахождения разности с опорной строкой (ref_row)
+    // Функции для решения методом Гаусса-Жордана
     void RowElimination(vector<float>& row, vector<float>& ref_row, float element) {
 
         for (int i = 0; i < row.size(); i++) {
             row[i] = row[i] - ref_row[i] * element;
         }
     }
-
-    // Функция для деления строки на ведущий элемент
     void RowDivision(vector<float>& row, float element) {
         for (int i = 0; i < row.size(); i++) {
             row[i] = row[i] / element;
         }
     }
-
-    // Основная функция для выполнения вычислений
-    void Processing(vector<vector<float> >& a, double delta) {
+    void SolveMatrix(vector<vector<float> >& a, double delta) {
         for (int k = 0; k < a.size(); k++) {
             if (abs(a[k][k]) < delta) {                  // Проверка.
                 for (int i = k + 1; i < a.size(); i++) { // Если элемент
@@ -82,162 +85,156 @@ const int a_size = 3;
         }
     }
 
-void MasterThread() {
-    int numProc;
-    MPI_Comm_size(MPI_COMM_WORLD, &numProc); // Кол-во процессов
-
-    // Создание матрицы А
-//float a_matrix_v1[a_size][a_size];
-    vector<vector<float>> a_matrix_v1(a_size);
-    // Заполнение матрицы случайными элементами (симметрично)
-    for (int i = 0; i < a_size; i++) {
-        for (int j = 0; j < a_size; j++) {
-            a_matrix_v1[i].push_back(rand() % 10); // заполняем строки элементами
-        }                                 // a[i][j] = {0, 9}
-    }
-    ShowMatrix(a_matrix_v1);
-    // Транспонирование матрицы
-    vector<vector<float>> a_matrix_v2(a_size, vector <float>(a_size));
-    for (int i = 0; i < a_size; i++) {
-        for (int j = 0; j < a_size; j++) {
-            a_matrix_v2[i][j] = a_matrix_v1[j][i];
+    //Функция для генерации и преобразования матрицы A
+    void GenMatA(vector<vector <float>> &a_matrix){
+        // Создание матрицы А
+        vector<vector<float>> a_matrix_v1(a_matrix.size());
+        // Заполнение матрицы случайными элементами (симметрично)
+        for (int i = 0; i < a_matrix.size(); i++) {
+            for (int j = 0; j < a_matrix.size(); j++) {
+                a_matrix_v1[i].push_back(rand() % 10); // заполняем строки элементами
+            }                                 // a[i][j] = {0, 9}
         }
-    }
-    ShowMatrix(a_matrix_v2);
-    float result = 0;
-    vector<vector<float>> a_matrix(a_size, vector <float>(a_size));
-    //Fill each cell of the matrix output.
-    for (int i = 0; i < a_size; i++) {
-        for (int j = 0; j < a_size; j++) {
-            //Multiply each row of matrix 1 with each column of matrix 2.
-            for (int k = 0; k < a_size; k++) {
-                result += a_matrix_v1[i][k] * a_matrix_v2[k][j];
+        ShowMatrix(a_matrix_v1);
+        // Транспонирование матрицы
+        vector<vector<float>> a_matrix_v2(a_matrix.size(), vector <float>(a_matrix.size()));
+        for (int i = 0; i < a_matrix.size(); i++) {
+            for (int j = 0; j < a_matrix.size(); j++) {
+                a_matrix_v2[i][j] = a_matrix_v1[j][i];
             }
-            a_matrix[i][j] = result;
-            result = 0; //Reset;
         }
+        ShowMatrix(a_matrix_v2);
+        float result = 0;
+        //Fill each cell of the matrix output.
+        for (int i = 0; i < a_matrix.size(); i++) {
+            for (int j = 0; j < a_matrix.size(); j++) {
+                //Multiply each row of matrix 1 with each column of matrix 2.
+                for (int k = 0; k < a_matrix.size(); k++) {
+                    result += a_matrix_v1[i][k] * a_matrix_v2[k][j];
+                }
+                a_matrix[i][j] = result;
+                result = 0; //Reset;
+            }
+        }
+        cout << "Matrix A:\n";
+        ShowMatrix(a_matrix);
     }
-    cout << "Matrix in master\n";
-    ShowMatrix(a_matrix);
- 
 
-    // Реализация расчётом методом квадратных корней (Разложение Холецкого)
-    vector<vector<float>> l_matrix(a_size, vector <float>(a_size));
-            // Создание пустой матрицы L
-            for (int i = 0; i < a_size; i++) {
-                for (int j = 0; j < a_size; j++) {
-                    l_matrix[i][j] = 0;
-                }
-            }
-            // Копирование в неё нижней треугольной матрицы из А
-            for (int i = 0; i < a_size; i++) {
-                for (int j = 0; j <= i; j++) {
-                    l_matrix[i][j] = a_matrix[i][j];
-                }
-            }
+    //Функция для нахождения матрицы L методом Холецкого
+    void FindMatL(vector<vector<float>> &l_matrix) {
+        int numProc, rank;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Ранг текущего процесса
+        MPI_Comm_size(MPI_COMM_WORLD, &numProc); // Кол-во процессов
+        if (rank == 0) {
             // Вычисление матрицы L методом Хоолецкого
-            for (int j = 0; j < a_size; j++) {
+            for (int j = 0; j < l_matrix.size(); j++) {
                 for (int k = 0; k < j; k++) {
-                    for (int i = j; i < a_size; i++) {
+                    for (int i = j; i < l_matrix.size(); i++) {
                         l_matrix[i][j] = l_matrix[i][j] - l_matrix[i][k] * l_matrix[j][k];
                     }
                 }
-            l_matrix[j][j] = sqrt(l_matrix[j][j]);
-            for (int i = j + 1; i < a_size; i++) {
-                l_matrix[i][j] = l_matrix[i][j] / l_matrix[j][j];
+                l_matrix[j][j] = sqrt(l_matrix[j][j]);
+                for (int i = j + 1; i < l_matrix.size(); i++) {
+                    l_matrix[i][j] = l_matrix[i][j] / l_matrix[j][j];
+                }
             }
-            }
-           cout << "L_matrix\n";
-           ShowMatrix(l_matrix); // получили нижнюю треугольную матрицу
-
-    // Добавление вектора b к полученной матрице
-    vector <float> b_vector(a_size);
-    for (int j = 0; j < a_size; j++) {
-        b_vector[j] = rand() % 10; // заполняем строки элементами
-    }
-    ShowVector(b_vector);
-
-    vector<vector<float>> lb_matrix(a_size, vector <float>(a_size+1));
-    for (int i = 0; i < a_size; i++) {
-        for (int j = 0; j < a_size + 1; j++) {
-            if (j == a_size) {
-                lb_matrix[i][j] = b_vector[i];
-                continue;
-            }
-            lb_matrix[i][j] = l_matrix[i][j];
+            cout << "SOLVED L_Matrix\n";
+            ShowMatrix(l_matrix); // получили нижнюю треугольную матрицу
         }
     }
-    cout << "LB_matrix\n";
-    ShowMatrixEx(lb_matrix);
+
+
+// Основная функция программы.
+void doKholesky(int argc, char** argv, int a_size) {
+    int numProc, rank;
+    MPI_Init(&argc, &argv); // MPI-инициализация
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Ранг текущего процесса
+    MPI_Comm_size(MPI_COMM_WORLD, &numProc); // Кол-во процессов
+    
+    //Создание симметричной положительно определённой матрицы А
+    vector<vector<float>> a_matrix(a_size, vector <float>(a_size));
+    if (rank == 0) GenMatA(a_matrix);
+
+    // Копирование в матрицу L нижней треугольной матрицы из А
+    // Основные вычисления для разложения Холецкого - поиск матрицы L
+    vector<vector<float>> l_matrix(a_size, vector <float>(a_size));
+        for (int i = 0; i < a_size; i++) {
+            for (int j = 0; j <= i; j++) {
+                l_matrix[i][j] = a_matrix[i][j];
+            }
+        }
+        if (rank == 0) ShowMatrix(l_matrix);
+        FindMatL(l_matrix);
+
+    // Создание вектора b (Выполняется главны потоком)
+    vector <float> b_vector(a_size);
+    if (rank == 0) {
+        for (int j = 0; j < a_size; j++) {
+            b_vector[j] = rand() % 10; // заполняем строки элементами
+        }
+        cout << "Vector [b]:\n";
+        ShowVector(b_vector);
+    }
+
+    // Добавление вектора b к СЛАУ: [LY = B] (Выполняется главны потоком)
+    vector<vector<float>> lb_matrix(a_size, vector <float>(a_size+1));
+    if (rank == 0){
+        for (int i = 0; i < a_size; i++) {
+            for (int j = 0; j < a_size + 1; j++) {
+                if (j == a_size) {
+                    lb_matrix[i][j] = b_vector[i];
+                    continue;
+                }
+                lb_matrix[i][j] = l_matrix[i][j];
+            }
+        }
+        cout << "[LY = B] matrix\n";
+        ShowMatrixEx(lb_matrix);
+    }
 
     double delta = 1e-08;
-    Processing(lb_matrix, delta);
-    cout << "LB_matrix_solved\n";
-    ShowMatrixEx(lb_matrix);
+    if (rank == 0) {
+        SolveMatrix(lb_matrix, delta);
+        cout << " SOLVED [LY = B] matrix\n";
+        ShowMatrixEx(lb_matrix);
+    }
 
-    
-    
-    // транспонирование матрицы L
+    // транспонирование матрицы L (Выполняется главны потоком)
     vector<vector<float>> lt_matrix(a_size, vector <float>(a_size));
-    for (int i = 0; i < a_size; i++) {
-        for (int j = 0; j < a_size; j++) {
-            lt_matrix[i][j] = l_matrix[j][i];
-        }
-    }
-    vector<vector<float>> lx_matrix(a_size, vector <float>(a_size + 1));
-    for (int i = 0; i < a_size; i++) {
-        for (int j = 0; j < a_size + 1; j++) {
-            if (j == a_size) {
-                lx_matrix[i][j] = lb_matrix[i][j];
-                continue;
+    if (rank == 0) {
+        for (int i = 0; i < a_size; i++) {
+            for (int j = 0; j < a_size; j++) {
+                lt_matrix[i][j] = l_matrix[j][i];
             }
-            lx_matrix[i][j] = lt_matrix[i][j];
         }
+        vector<vector<float>> lx_matrix(a_size, vector <float>(a_size + 1));
+        for (int i = 0; i < a_size; i++) {
+            for (int j = 0; j < a_size + 1; j++) {
+                if (j == a_size) {
+                    lx_matrix[i][j] = lb_matrix[i][j];
+                    continue;
+                }
+                lx_matrix[i][j] = lt_matrix[i][j];
+            }
+        }
+        cout << "[LtX = Y] matrix\n";
+        ShowMatrixEx(lx_matrix);
+        SolveMatrix(lx_matrix, delta); // Решение СЛАУ методом Гаусса-Жордана
+        cout << "SOLVED [LtX = Y] matrix\n";
+        ShowMatrixEx(lx_matrix); // Вывод конечной единичной матрицы
+        ShowResult(lx_matrix); // Вывод решения СЛАУ Ax = b
     }
-    cout << "Lx_matrix\n";
-    ShowMatrixEx(lx_matrix);
 
-    Processing(lx_matrix, delta);
-    cout << "LX_matrix_solved\n";
-    ShowMatrixEx(lx_matrix);
-
-
-    // Отправление матрицы А второстепенным процессам
-    for (auto i = 1; i < numProc; i++)
-    {
-   //     MPI_Send(a_matrix, sizeof(a_matrix), MPI_FLOAT, i, 0, MPI_COMM_WORLD);
-    }
-    
+    MPI_Finalize(); // Завершение MPI программы
 }
 
-void SlaveThread() {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Ранг текущего процесса
-    
-    float recBufA[a_size][a_size];
- //   MPI_Recv(recBufA, sizeof(recBufA), MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-    //cout << "Matrix in slave numba" << rank << endl;
-    //ShowMatrix(recBufA);
-    
 
-}
-
-// Задание размера массива
 
 int main(int argc, char** argv)
 {
-    int rank, numProc;
-	MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); // Ранг текущего процесса
-    MPI_Comm_size(MPI_COMM_WORLD, &numProc); // Кол-во процессов
-
-    if (rank == 0) MasterThread();// Работа главного master потока
-    else SlaveThread();// Работа ведомых slave потоков
-
-
-	MPI_Finalize();
-
+    int a_size = 3;
+    doKholesky(argc, argv, a_size);
 	return 0;
 }
 
